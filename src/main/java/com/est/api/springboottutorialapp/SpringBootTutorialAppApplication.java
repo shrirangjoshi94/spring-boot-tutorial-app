f667e2f9445e4d40235be4e03da062780ec3006f
Shrirang Joshi <shrirangjoshi94@gmail.com>
2024-04-09 21:42:52 +0530

    first commit

---
+++ src/main/java/com/est/api/springboottutorialapp/SpringBootTutorialAppApplication.java
@@ -0,0 +1,13 @@
+package com.est.api.springboottutorialapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutorialAppApplication.class, args);
+	}
+
+}
